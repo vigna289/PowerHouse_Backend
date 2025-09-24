1a52126e818ece9f97d39026a9add26d81df0849
vigna567 <vigna298@gmail.com>
2025-09-24 23:17:56 +0530

    testing changes

--- src/Config/razorPayClient.js
+++ src/Config/razorPayClient.js
@@ -1,9 +1,17 @@
+// const Razorpay = require("razorpay");
+// apiKey= "rzp_test_RLPgLxrvh3CrS6"
+// apiSecret="K0bYLyFLAgJZlCqwttMqi7xy"
+// const razorpay = new Razorpay({
+//   key_id:apiKey,
+//   key_secret: apiSecret,
+// });
+
+// module.exports=razorpay;
 const Razorpay = require("razorpay");
-apiKey= "rzp_test_RLPgLxrvh3CrS6"
-apiSecret="K0bYLyFLAgJZlCqwttMqi7xy"
+
 const razorpay = new Razorpay({
-  key_id:apiKey,
-  key_secret: apiSecret,
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
-module.exports=razorpay;
+module.exports = razorpay;
