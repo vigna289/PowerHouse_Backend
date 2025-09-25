7e1989a62164b9d9efe0a8b8c1b072225dd0a5ea
vigna567 <vigna298@gmail.com>
2025-09-25 23:24:13 +0530

    trying on render

--- src/Config/db.js
+++ src/Config/db.js
@@ -1,9 +1,19 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-const mongodbUrl="mongodb+srv://vigna298_db_user:Uk658JhexxSG6m4B@cluster0.hf9mqu9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+// const mongodbUrl="mongodb+srv://vigna298_db_user:Uk658JhexxSG6m4B@cluster0.hf9mqu9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-const connectDb=()=>{
-    return mongoose.connect(mongodbUrl);
-}
+// const connectDb=()=>{
+//     return mongoose.connect(mongodbUrl);
+// }
 
-module.exports={connectDb}
+// module.exports={connectDb}
+const mongoose = require("mongoose");
+
+// Get MongoDB URL from environment variable
+const mongodbUrl = process.env.MONGODB_URL;
+
+const connectDb = () => {
+  return mongoose.connect(mongodbUrl);
+};
+
+module.exports = { connectDb };
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const app = require(".");
 const { connectDb } = require("./Config/db");
 
